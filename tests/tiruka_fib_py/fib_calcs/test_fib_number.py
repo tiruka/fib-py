af245b7e3f33aacc822958b002b52cc539cfe721
tiruka <tiruka.engineer@gmail.com>
2024-05-30 11:57:41 +0900

    update tests

--- tests/tiruka_fib_py/fib_calcs/test_fib_number.py
+++ tests/tiruka_fib_py/fib_calcs/test_fib_number.py
@@ -2,7 +2,7 @@ from unittest import main, TestCase
 from tiruka_fib_py.fib_calcs.fib_number import rec_fibonacci_numbers
 
 
-class RecFibNumberTest(TestCase):
+class TestRecFibNumber(TestCase):
     def test_zero(self):
         self.assertEqual(0, rec_fibonacci_numbers(0))
 
